b260557024cca1fe9d3b18f02ffd9c07944777a5
Jason Minhas <jasonsminhas@gmail.com>
2023-12-23 19:29:39 -0500

    created stock class and

---
+++ classes.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import general_functions as gf
+import yfinance as yf
+
+
+def get_data(ticker):
+    stock_ticker = yf.Ticker(ticker)
+    hist_stock_df = stock_ticker.history(period="max")
+
+    return hist_stock_df
+
+
+def clean_stock_data(hist_stock_df):
+    hist_stock_df = hist_stock_df.reset_index()
+    hist_stock_df = gf.clean_headers(hist_stock_df)
+    hist_stock_df = gf.strip_df(hist_stock_df)
+    hist_stock_df['date'] = hist_stock_df['date'].apply(lambda x: pd.to_datetime(x))
+
+    return hist_stock_df
+
+
+class StockDataFrameClass:
+    def __init__(self, ticker):
+        self.df = clean_stock_data(get_data(ticker))
+        self.start_date = min(self.df['date'])
+        self.end_date = max(self.df['date'])
+
+    def calculate_return(self, start_date, end_date):
+        # filter for relevant timeframe
+        temp_df = self.df[(self.df['date'] >= start_date) & (self.df['date'] <= end_date)]
+        # take only first and last row which would be the min and max date
+        temp_df = temp_df.iloc[[0, -1]]
+        # find percentage difference between the min and max date
+        return_pct = temp_df['close'].pct_change()
+
+        return return_pct
+
+    def add_column_for_best_x_days(self, number_of_days_to_remove, start_date, end_date):
+        # filter for relevant timeframe
+        temp_df = self.df[(self.df['date'] >= start_date) & (self.df['date'] <= end_date)].copy(deep=True)
+        # create new series for the percent change between all the rows
+        temp_df['pct_returns_vs_prev_day'] = temp_df['close'].pct_change().fillna(0)
+        temp_df['pct_alternate_returns_vs_prev_day'] = temp_df['pct_returns_vs_prev_day']
+        # Get indices of top x rows
+        top_x_indices = temp_df.nlargest(number_of_days_to_remove, 'pct_returns_vs_prev_day').index
+        # replace top x indices with 0 and store in alternate series
+        temp_df['pct_alt_returns_vs_prev_day'] = temp_df['pct_returns_vs_prev_day']
+        temp_df.loc[top_x_indices, 'pct_alt_returns_vs_prev_day'] = 0
+
+        # calculate alternate close price
+        temp_value = temp_df.iloc[0]['close']
+        for index, row in temp_df.iterrows():
+            alt_return_pct = row['pct_alt_returns_vs_prev_day']
+            temp_value = temp_value + (temp_value * alt_return_pct)
+            temp_df.at[index, 'alt_close'] = temp_value
+
+        self.df = temp_df
--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-# practice based on https://www.dataquest.io/blog/portfolio-project-predicting-stock-prices-using-pandas-and-scikit-learn/
-
 import pandas as pd
+import general_functions as gf
 import datetime as dt
 import os
+import classes as sc
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-stonk_ticker = yf.Ticker("SPY")
-stonk_hist_df = stonk_ticker.history(period="max")
-stonk_hist_df = stonk_hist_df.reset_index()
-stonk_hist_df['Date'] = stonk_hist_df['Date'].apply(lambda x: pd.to_datetime(x)).dt.date
-stonk_hist_df.to_excel("historical_stonk_data/SPY_stonk_data.xlsx", index=False)
+
+def main():
+    hist_stock = sc.StockDataFrameClass("SPY")
+    hist_stock = run_scenario(hist_stock)
+    hist_stock = visualize_stock_df(hist_stock)
 
 
-# stonk_hist_df = pd.read_csv("historical_stonk_data/MSFT_stonk_data.csv", index_col='Date')
+def run_scenario(hist_stock):
+    hist_stock.add_column_for_best_x_days(10, '2008-01-02', '2023-12-21')
+
+    return hist_stock
 
 
-# stonk_hist_df.plot.line(y="Close")
-# plt.show()
+def visualize_stock_df(hist_stock):
+    hist_stock.df.plot.line(y=['close', 'alt_close'])
+    plt.show()
 
 
-print('yolo')
+if __name__ == "__main__":
+    main()
