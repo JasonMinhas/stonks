aa5377b3655df06b9998c7abdeecf468082810b7
Jason Minhas <jasonsminhas@gmail.com>
2023-12-26 23:25:00 -0500

    brainstorming capturing scenarios in

--- test.py
+++ test.py
@@ -1,96 +1,47 @@
-from dataclasses import dataclass, field
-import datetime as dt
+from dataclasses import dataclass
 import pandas as pd
-import general_functions as gf
-import yfinance as yf
-from dateutil.relativedelta import relativedelta
-from typing import Optional
 
-
-def get_stock_data(ticker):
-    stock_ticker = yf.Ticker(ticker)
-    hist_stock_df = stock_ticker.history(period="max")
-
-    return hist_stock_df
-
-
-def clean_stock_data(hist_stock_df):
-    hist_stock_df = hist_stock_df.reset_index()
-    hist_stock_df = gf.clean_headers(hist_stock_df)
-    hist_stock_df = gf.strip_df(hist_stock_df)
-    hist_stock_df['date'] = pd.to_datetime(hist_stock_df['date']).dt.date
-
-    return hist_stock_df
-
-
+# Define your data class to represent a scenario
 @dataclass
-class StockData():
-    ticker: str
-    start_date: dt.date
-    end_date: dt.date
-    stock_df: Optional[pd.DataFrame] = None
-    alt_stock_df: Optional[pd.DataFrame] = None
-    top_x_indices_removed: Optional[pd.Index] = None
+class StockScenario:
+    name: str
+    multiplier: float
+    additional_data: dict
 
-    # todo property that identifies whether this is the orginal data or it has been changed
+# Function to apply a scenario to the stock data
+def apply_scenario(stock_data, scenario):
+    # Modify stock data based on the scenario
+    modified_data = stock_data.copy()
+    modified_data['Close'] = modified_data['Close'] * scenario.multiplier
 
-    def periods_year(self) -> int:
-        return relativedelta(self.end_date, self.start_date).years
+    # Additional scenario-specific modifications
+    # You can customize this based on your specific needs
+    for key, value in scenario.additional_data.items():
+        modified_data[key] = modified_data[key] + value
 
-    # Calculate return percentage
-    def get_return_pct(self) -> float:
-        # take only first and last row which would be the min and max date
-        first_and_last_rows = self.stock_df.iloc[[0, -1]]
-        # find percentage difference between the min and max date
-        return_pct = first_and_last_rows['close'].pct_change().iloc[-1]
+    return modified_data
 
-        return return_pct
+# Example usage
+if __name__ == "__main__":
+    # Load your historical stock data into a DataFrame
+    # Replace this with your actual data loading code
+    stock_data = pd.DataFrame({
+        'Date': pd.date_range(start='2023-01-01', end='2023-01-10'),
+        'Close': [100, 105, 110, 95, 98, 102, 108, 112, 98, 105],
+        'Volume': [554100, 104545, 1168970, 96546455, 435598, 103452, 108345, 112453, 9435458, 104535]
+    })
 
-    # Calculate annualized return percentage
-    def get_annualized_return_pct(self) -> float:
-        self.stock_df.annualized_return_pct = (1 + self.stock_df.return_pct) ** (1 / self.periods_year()) - 1
+    # Define different scenarios using the StockScenario data class
+    # This scenario could be my base in which the parameter is top 0 days so nothing changes
+    scenario1 = StockScenario(name='Scenario 1', multiplier=1.1, additional_data={'Volume': 1000})
+    scenario2 = StockScenario(name='Scenario 2', multiplier=0.9, additional_data={'Volume': -500})
 
-    # todo create method to simulate skipping best x days
-    def get_close_series_with_best_x_days_removed(self, number_of_days_to_remove):
-        # create alt DataFrame that will hold new returns_pct_vs_prev_day and close values
-        self.alt_stock_df = self.stock_df[['returns_pct_vs_prev_day']]
-        self.alt_stock_df = self.alt_stock_df.add_prefix('alt_')
-        # Get indices of top x rows
-        self.top_x_indices_removed = self.alt_stock_df.nlargest(number_of_days_to_remove, 'alt_returns_pct_vs_prev_day').index
-        # replace top x indices with 0 and store in alternate series
-        self.alt_stock_df.loc[self.top_x_indices_removed] = 0
+    # Apply scenarios and store modified dataframes in a dictionary
+    scenario_results = {}
+    for scenario in [scenario1, scenario2]:
+        modified_data = apply_scenario(stock_data, scenario)
+        scenario_results[scenario.name] = modified_data
 
-        # calculate alternate close price
-        temp_value = self.stock_df.iloc[0]['close']
-        for index, row in self.alt_stock_df.iterrows():
-            alt_return_pct = row['alt_returns_pct_vs_prev_day']
-            temp_value = temp_value + (temp_value * alt_return_pct)
-            self.alt_stock_df.at[index, 'alt_close'] = temp_value
-
-    def __post_init__(self):
-        # check if end date is after start date
-        if self.periods_year() < 0:
-            raise ValueError(
-                f"The start_date argument, {self.start_date}, should be before the end_date argument, {self.end_date}")
-        # todo add max/min dates for complete stock dataset. Then creates a method to repull the stock_df with new dates
-        # get stock data
-        stock_df = get_stock_data(self.ticker)
-        stock_df = clean_stock_data(stock_df)
-        self.stock_df = stock_df[
-            (stock_df['date'] >= self.start_date)
-            & (stock_df['date'] <= self.end_date)
-            ]
-
-        # get return percentage compared to previous day
-        self.stock_df['returns_pct_vs_prev_day'] = self.stock_df['close'].pct_change().fillna(0)
-
-
-stock1 = StockData(
-    'PATH',
-    dt.date(2015, 1, 1),
-    dt.date(2023, 11, 30)
-)
-
-print(stock1.get_close_series_with_best_x_days_removed(5))
-
-print('stop')
+    # Access modified dataframes for each scenario
+    for scenario_name, modified_data in scenario_results.items():
+        print(f"\nResults for {scenario_name}:\n{modified_data}")
